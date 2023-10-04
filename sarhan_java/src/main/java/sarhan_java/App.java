652dd48d9c5770b4202e3d96696d8ed229f13dd1
Anton <anton@antons-MacBook-Air.local>
2023-10-04 12:41:23 +0300

    added project structure

---
+++ sarhan_java/src/main/java/sarhan_java/App.java
@@ -0,0 +1,13 @@
+package sarhan_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
