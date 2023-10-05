f0bdc8267ff02bfee7d8fbfba301bfc6fde01961
Anton <anton@antons-MacBook-Air.local>
2023-10-05 13:21:51 +0300

    done

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Sarhan_Java
 
-java sarhan_java/src/main/java/sarhan_java/App.java  
+Чтобы скомпилировать и выполнить код, проделайте следующие шаги (MacOS, Linux)
+
+1. Склонируйте репозиторий
+
+2. Находясь в папке с .git репозиторием, перейдите в папку с кодом
+
+```
+cd sarhan_java
+```
+
+3.  Создайте .jar пакет через инструмент автоматизации проектов [Maven](https://maven.apache.org/download.cgi)
+
+```
+mvn package
+```
+
+4. Выполните следующую команду
+
+```
+java -cp target/sarhan_java-1.0-SNAPSHOT.jar sarhan_java.App
+```
+
--- sarhan_java/src/main/java/sarhan_java/Task3.java
+++ sarhan_java/src/main/java/sarhan_java/Task3.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class Task3 extends AbstractTask{
 
-	private final String variableTypeString = "последовательность чисел в форматe: \n1,2,3,4";
+	private final String variableTypeString = "последовательность чисел без пробелов в форматe: \n1,2,3,4 ";
 	private final String regexArrayPatternString = "^[\\d,]+";
 	private String[] inputArray;
 
 	private void divisibleOfArray(String[] inputString) {
-		ArrayList resultArray = new ArrayList<Integer>();
-		for (String intString : inputString) {
-			int i = Integer.valueOf(intString);
+		ArrayList resultArray = new ArrayList<Float>();
+		for (String numberString : inputString) {
+			int i = Integer.valueOf(numberString);
 			if (i % 3 == 0 && i != 0) {
 				resultArray.add(i);
 			}
